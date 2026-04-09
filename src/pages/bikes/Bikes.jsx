c1faf025e56e2c32f193d9a86f4d60be8a3be9c1
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-04-09 22:09:15 +0200

    adjust grid size

--- src/pages/bikes/Bikes.jsx
+++ src/pages/bikes/Bikes.jsx
@@ -34,6 +34,56 @@ export function Bikes() {
           <h2>Bike Name</h2>
           <p>€ 2100</p>
         </div>
+        <div className={styles.bikeCard}>
+          <i className={styles.bikeType}>Road</i>
+          <img
+            className={styles.bikeImg}
+            src="https://asset.scott-sports.com/fit-in/260x260/425/4253488087_2223048_3.png?signature=991b49b7a60172fca3ba8def142f86b71122c7baf1937421157c38e1ba4b8502"
+            alt="image of bike"
+          />
+          <h2>Bike Name</h2>
+          <p>€ 2100</p>
+        </div>
+        <div className={styles.bikeCard}>
+          <i className={styles.bikeType}>Road</i>
+          <img
+            className={styles.bikeImg}
+            src="https://asset.scott-sports.com/fit-in/260x260/425/4253488087_2223048_3.png?signature=991b49b7a60172fca3ba8def142f86b71122c7baf1937421157c38e1ba4b8502"
+            alt="image of bike"
+          />
+          <h2>Bike Name</h2>
+          <p>€ 2100</p>
+        </div>
+        <div className={styles.bikeCard}>
+          <i className={styles.bikeType}>Road</i>
+          <img
+            className={styles.bikeImg}
+            src="https://asset.scott-sports.com/fit-in/260x260/425/4253488087_2223048_3.png?signature=991b49b7a60172fca3ba8def142f86b71122c7baf1937421157c38e1ba4b8502"
+            alt="image of bike"
+          />
+          <h2>Bike Name</h2>
+          <p>€ 2100</p>
+        </div>
+        <div className={styles.bikeCard}>
+          <i className={styles.bikeType}>Road</i>
+          <img
+            className={styles.bikeImg}
+            src="https://asset.scott-sports.com/fit-in/260x260/425/4253488087_2223048_3.png?signature=991b49b7a60172fca3ba8def142f86b71122c7baf1937421157c38e1ba4b8502"
+            alt="image of bike"
+          />
+          <h2>Bike Name</h2>
+          <p>€ 2100</p>
+        </div>
+        <div className={styles.bikeCard}>
+          <i className={styles.bikeType}>Road</i>
+          <img
+            className={styles.bikeImg}
+            src="https://asset.scott-sports.com/fit-in/260x260/425/4253488087_2223048_3.png?signature=991b49b7a60172fca3ba8def142f86b71122c7baf1937421157c38e1ba4b8502"
+            alt="image of bike"
+          />
+          <h2>Bike Name</h2>
+          <p>€ 2100</p>
+        </div>
       </div>
     </main>
   );
