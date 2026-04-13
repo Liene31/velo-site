3ba9624fa5719a22ff6db443af1cd1195a8d7b64
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-04-13 20:29:30 +0200

    fix bike card arrangement on mobile screens

--- src/layout/components/Footer.jsx
+++ src/layout/components/Footer.jsx
@@ -1,3 +1,3 @@
 export function Footer() {
-  return <footer>&#169; 2026 Ride Into the Future</footer>;
+  return <footer>&#169; 2026 Bike to the Future</footer>;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function Home() {
   return (
     <main className={styles.hero}>
-      <h1 className={styles.heroTitle}>Ride Into The Future</h1>
+      <h1 className={styles.heroTitle}>Bike to the Future</h1>
       <Link className={styles.heroCta} to={"bikes"}>
         Explore our Bikes
       </Link>
