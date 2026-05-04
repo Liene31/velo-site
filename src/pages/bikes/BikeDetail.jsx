ba2a3baca37d7a33bb3f8d32d7c6060997b5435b
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-05-04 14:49:33 +0200

    add data fetch and error/loading handling

--- src/pages/bikes/BikeDetail.jsx
+++ src/pages/bikes/BikeDetail.jsx
@@ -1,46 +1,73 @@
 import { useEffect, useState } from "react";
 import styles from "./BikeDetail.module.css";
 import { NavLink, Outlet, useParams } from "react-router-dom";
+import { bikeService } from "../../services/bike.service";
 
 export function BikeDetail() {
   const [currentBike, setCurrentBike] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`/data/bikes.json`)
-      .then((res) => res.json())
+    setIsLoading(true);
+    setError(null);
+    bikeService
+      .getById(id)
       .then((data) => {
-        const filteredBike = data.filter((bike) => {
-          return bike.id === id;
-        });
-
-        setCurrentBike(filteredBike[0]);
+        setCurrentBike(data);
+        setIsLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          // server responded (e.g. 500 with "DB error")
+          setError(error.response.data.message);
+        } else if (error.request) {
+          // request made but no response (server down / network issue)
+          setError("Server unavailable, please try again later");
+        } else {
+          // something else went wrong
+          setError("Unexpected error, please try again later");
+        }
+        setIsLoading(false);
       });
-  }, []);
+    //use id as dependency because data should re-load if id is changing
+  }, [id]);
+
+  //checks if no errors and data is loaded
+  // if conditions are not met/or is met, shows appropriate message
+  let bikeDetailContent;
+
+  if (error) {
+    bikeDetailContent = <p>{error}</p>;
+  } else if (isLoading) {
+    bikeDetailContent = <p>Loading</p>;
+  } else if (!currentBike) {
+    bikeDetailContent = <p>Bike not found</p>;
+  } else {
+    bikeDetailContent = (
+      <>
+        <img
+          className={styles.bikeImg}
+          src={currentBike.images[0].url}
+          alt={currentBike.images[0].alt}
+        />
+        <div className={styles.bikeInfo}>
+          <i>{currentBike.overview.category}</i>
+          <h1>{currentBike.name}</h1>
+          <p>
+            {currentBike.overview.currency} {currentBike.overview.price}
+          </p>
+        </div>
+      </>
+    );
+  }
 
   return (
     <main className={styles.bikesDetailPage}>
       <div className={styles.bikeDetailWrapper}>
-        <div className={styles.bikeTopInfo}>
-          {currentBike ? (
-            <>
-              <img
-                className={styles.bikeImg}
-                src={currentBike.images[0].url}
-                alt={currentBike.images[0].alt}
-              />
-              <div className={styles.bikeInfo}>
-                <i>{currentBike.overview.category}</i>
-                <h1>{currentBike.name}</h1>
-                <p>
-                  {currentBike.overview.currency} {currentBike.overview.price}
-                </p>
-              </div>
-            </>
-          ) : (
-            <h2>Loading</h2>
-          )}
-        </div>
+        <div className={styles.bikeTopInfo}>{bikeDetailContent}</div>
         <nav className={styles.navLinks}>
           <NavLink to="." end>
             Overview
--- src/services/bike.service.js
+++ src/services/bike.service.js
@@ -12,4 +12,15 @@ export const bikeService = {
       throw err; // rethrow
     }
   },
+
+  getById: async (id) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/bikes/${id}`);
+      return response.data;
+      console.log(response);
+    } catch (err) {
+      console.error("Bike fetch failed:", err);
+      throw err; // rethrow
+    }
+  },
 };
