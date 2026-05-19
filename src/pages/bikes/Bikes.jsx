e821646e19423620b924124033b60a31c603c837
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-05-19 21:07:04 +0200

    add dynamically bike tags to filter

--- src/pages/bikes/Bikes.jsx
+++ src/pages/bikes/Bikes.jsx
@@ -51,7 +51,29 @@ export function Bikes() {
     );
   });
 
-  console.log(bikes);
+  //gets all the bike tags arrays in one array (nested array)
+  const bikeTagArray = bikes?.map((bike) => {
+    return bike.tags;
+  });
+
+  //flatten the nested array (transforms into one array)
+  const flattenedTagArray = bikeTagArray.flat(Infinity);
+
+  //create set of unique values using Set constructor.
+  //it's an object
+  const uniqueTags = new Set(flattenedTagArray);
+
+  //convert the object back to array
+  const uniqueTagsArray = [...uniqueTags];
+
+  //display filter list
+  const filterTags = uniqueTagsArray.map((tag) => {
+    return (
+      <option key={tag} value={tag}>
+        {tag}
+      </option>
+    );
+  });
 
   //checks if no errors and data is loaded
   // if conditions are not met/or is met, shows appropriate message
@@ -82,11 +104,7 @@ export function Bikes() {
           defaultValue="all"
         >
           <option value="all">All Bikes</option>
-          <option value="road">Road</option>
-          <option value="gravel">Gravel</option>
-          <option value="commuter">Commuter</option>
-          <option value="lightweight">Lightweight</option>
-          <option value="ebike">E-bike</option>
+          {filterTags}
         </select>
       </section>
       <div className={styles.bikesWrapper}>{bikeContent}</div>
